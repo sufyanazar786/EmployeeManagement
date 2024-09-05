42cbde161d890cc636c3241c5136850949077fbb
Sufyan Azar <66428774+sufyanazar786@users.noreply.github.com>
2024-09-06 00:19:55 +0530

    Delete FullStackWebApp/backend/employee-system/employee-system directory

--- FullStackWebApp/backend/employee-system/employee-system/src/main/java/com/springproject/employee_system/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springproject.employee_system.repository;
-
-import com.springproject.employee_system.entity.EmployeeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
-}
